95b77cea9bfa931103271c0862bb73ff7c50e783
zhangqiao <qiaoqiao10001@gmail.com>
2019-12-26 15:49:39 +0800

    feat react 16 新特性

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import PostList from './PostList'
+import Modal from './components/Modal'
 // import FormInput from './components/FormInput'
 // import FormSelect from './components/FormSelect'
 // import FormRadio from './components/FormRadio'
 
-function App() {
-  return (
-    <div className="App">
-      <PostList></PostList>
-      {/* <FormInput></FormInput>
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {showModal: false}
+  }
+  
+  closeModal=()=>{
+    this.setState({showModal: false})
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <PostList></PostList>
+        {this.state.showModal && (
+          <Modal onClose={this.closeModal}>modaldialog</Modal>
+        )}
+        {/* <FormInput></FormInput>
       <FormSelect></FormSelect>
       <FormRadio></FormRadio> */}
-    </div>
-  );
+      </div>
+    )
+  };
 }
 
 export default App;
